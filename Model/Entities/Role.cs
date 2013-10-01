e4b643ef05fb860801bd79a2f698a6e16c73e363
Jamey McElveen <jamey.mcelveen@acstechnologies.com>
2013-10-01 19:26:49 -0400

    add role entity

---
+++ Model/Entities/Role.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Demo.Model.Entities
+{
+	public class Role
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
+
--- Model/Entities/User.cs
+++ Model/Entities/User.cs
@@ -10,7 +10,6 @@ namespace Demo.Model.Entities
 		public string Email { get; set; }
 		public string PasswordHash { get; set; }
 		public string PasswordSalt { get; set; }
-
 	}
 }
 
